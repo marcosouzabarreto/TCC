70f40c82fda9e294d30e714eddbc95d138bdb6b6
Marco Barreto <67713823+marcosouzabarreto@users.noreply.github.com>
2023-12-12 15:48:10 -0300

    Refactor

--- main.py
+++
@@ -1,8 +0,0 @@
-from AI import oracle
-
-oracle(
-    portfolio=["TSLA", "AAPL", "NVDA", "NFLX"],
-    start_date="2020-01-01",
-    weights=[0.3, 0.2, 0.3, 0.2],  # allocate 30% to TSLA and 20% to AAPL...(equal weighting  by default)
-    prediction_days=2  # number of days you want to predict
-)
